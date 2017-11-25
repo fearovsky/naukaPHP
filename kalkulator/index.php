c19243b33c920293c194cc357c7aec01b9348950
fearovsky <mateuszkrzak.web@gmail.com>
2017-11-25 17:01:47 +0100

    Stworzenie formularza

--- kalkulator/index.php
+++ kalkulator/index.php
@@ -6,5 +6,21 @@
 </head>
 <body>
 
+<form action="">
+	<input type="text" name="num1" placeholder="Liczba pierwsza">
+	<input type="text" name="num2" placeholder="Liczba druga">
+	<select name="operator">
+		<option>--</option>
+		<option>Dodawanie</option>
+		<option>Odejmowanie</option>
+		<option>Mnożenie</option>
+		<option>Dzielenie</option>
+	</select>
+
+	<br>
+
+	<button type="submit" name="submit" value="submit">Policz</button>
+</form>
+
 </body>
 </html>
