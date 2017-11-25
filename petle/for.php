33ef1ce56a753db31aad81101cde40829aae5173
fearovsky <mateuszkrzak.web@gmail.com>
2017-11-25 18:13:51 +0100

    Petla for, Kod

---
+++ petle/for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<title>Pętla for</title>
+</head>
+<body>
+
+<?php
+
+	for($x = 0; $x<=10 ;$x++){
+		echo $x;
+	}
+
+?>
+
+
+
+</body>
+</html>
