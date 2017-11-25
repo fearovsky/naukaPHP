179aaf36357a6cb8a25c430f533fae89dccb43fc
fearovsky <mateuszkrzak.web@gmail.com>
2017-11-25 17:12:20 +0100

    Stworzenie skryptu kalkulatora[PHP]

--- kalkulator/index.php
+++ kalkulator/index.php
@@ -6,9 +6,10 @@
 </head>
 <body>
 
-<form action="">
+<form>
 	<input type="text" name="num1" placeholder="Liczba pierwsza">
 	<input type="text" name="num2" placeholder="Liczba druga">
+
 	<select name="operator">
 		<option>--</option>
 		<option>Dodawanie</option>
@@ -21,6 +22,35 @@
 
 	<button type="submit" name="submit" value="submit">Policz</button>
 </form>
+
+<p>Wynik wynosi:</p>
+
+<?php
+	if(isset($_GET['submit'])){
+		$wynik1 = $_GET['num1'];
+		$wynik2 = $_GET['num2'];
+		$operator = $_GET['operator'];
+
+		switch($operator){
+			case "--":
+				echo "Nie wybrałeś operatora!";
+			break;
+			case "Dodawanie":
+				echo $wynik1 + $wynik2;
+			break;
+			case "Odejmowanie":
+				echo $wynik1 - $wynik2;
+			break;
+			case "Mnożenie":
+				echo $wynik1 * $wynik2;
+			break;
+			case "Dzielenie":
+				echo $wynik1 / $wynik2;
+			break;
+		}
+	}
+
+?>
 
 </body>
 </html>
