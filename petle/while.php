50724394849797fefefe2dea4560c079fb26dcde
fearovsky <mateuszkrzak.web@gmail.com>
2017-11-25 18:13:57 +0100

    Pętla while, Kod

---
+++ petle/while.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<title>Pętla while</title>
+</head>
+<body>
+
+<?php
+
+	$x = 1;
+
+	while($x <= 5){
+		echo "Działa... [$x]<br>";
+		$x++;
+	}
+
+?>
+
+</body>
+</html>
